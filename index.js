e68dca0356890a572c1e122ecc2f7973bb60f54a
Krishna <krishnadhallakd@gmail.com>
2022-10-31 01:18:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent=document.getElementsByClassName('skills_content'),
+skillHeader=document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+    
+}
+
+/*==================== QUALIFICATION TABS ====================*/
+
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portfolio_container', {
+    cssMode: true,
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+  });
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
